51f4b4d11b58585d591598aa1340a2f8d7c82ec1
lorangeve <lorangeve@gmail.com>
2026-02-09 02:56:19 +0800

    🚧 test github page

--- app.config.ts
+++ app.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from "@solidjs/start/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  ssr: false,
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
